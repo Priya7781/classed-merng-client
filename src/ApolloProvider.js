ba23ab8449137ec9cedf8a4dc59e270c80ffbb29
Priyadarshi <priyadarshinayak2001@gmail.com>
2022-02-03 12:42:10 +0530

    edited proxy and server base url

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:5000'
+  uri: 'https://stormy-brook-67023.herokuapp.com/'
 });
 
 const authLink = setContext(() => {
